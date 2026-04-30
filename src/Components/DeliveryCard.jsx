8e3a63578f5239856af49beec2f6551f4d469012
crispcodews <crispcodews@gmail.com>
2026-04-29 23:33:03 -0500

    CSS Styling Added

--- src/Components/DeliveryCard.jsx
+++ src/Components/DeliveryCard.jsx
@@ -1,19 +1,50 @@
-import styles from './DeliveryCard.module.css'
+import styles from "./DeliveryCard.module.css";
 
 function DeliveryCard({ delivery, onDelete }) {
-    return (
-        <div className={styles.card}>
-            <div className={styles.cardHeader}>
-                <h3>{DeliveryCard.recipient}</h3>
-                <button className={styles.deleteButton} onClick={() => onDelete(delivery.id)}>Delete</button> 
-            </div>
-            <p><strong>Tracking ID:</strong> {delivery.id}</p>
-            <p><strong>Address:</strong> {delivery.address}</p>
-            <p><strong>Carrier:</strong> {delivery.carrier}</p>
-           <p><strong>Status:</strong> {delivery.status}</p>
-           <p><strong>Est. Delivery:</strong>{delivery.estimatedDelivery}</p>
-        </div>
-    )
+  const getStatusClass = (status) => {
+    if (status === "Pending") return styles.statusPending;
+    if (status === "In Transit") return styles.statusInTransit;
+    if (status === "Delivered") return styles.statusDelivered;
+    return "";
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader}>
+        <h3>{delivery.recipient}</h3>
+        <button
+          className={styles.deleteButton}
+          onClick={() => onDelete(delivery.id)}
+        >
+          Delete
+        </button>
+      </div>
+      <div className={styles.info}>
+        <p>
+          <strong>Tracking ID:</strong> {delivery.id}
+        </p>
+        <p>
+          <strong>Carrier:</strong> {delivery.carrier}
+        </p>
+        <p>
+          <strong>Address:</strong> {delivery.address}
+        </p>
+        <p>
+          <strong>Est. Delivery:</strong> {delivery.estimatedDelivery}
+        </p>
+        <p>
+          <strong>Status:</strong>
+          <span
+            className={`${styles.statusBadge} ${getStatusClass(
+              delivery.status
+            )}`}
+          >
+            {delivery.status}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default DeliveryCard
+export default DeliveryCard;
